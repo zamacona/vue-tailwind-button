5a45ff49dab705231ac7cb1b785d9138188a4b10
Carlos Eymard <cezamacona@gmail.com>
2021-07-16 16:08:10 -0500

    okay

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,17 +1,40 @@
 <script>
-import { defineComponent } from 'vue';
-import VueTailwindButton from '@/vue-tailwind-button.vue';
-
+import { defineComponent } from "vue";
+import VueTailwindButton from "@/vue-tailwind-button.vue";
+import "./index.css";
 export default defineComponent({
-  name: 'ServeDev',
+  name: "ServeDev",
   components: {
-    VueTailwindButton
-  }
+    VueTailwindButton,
+  },
 });
 </script>
 
 <template>
-  <div id="app">
-    <vue-tailwind-button />
+  <div id="app" class="mt-10 mx-28">
+    <div class="grid justify-items-center grid-cols-10">
+      <vue-tailwind-button color="black"> black </vue-tailwind-button>
+      <vue-tailwind-button color="white"> white </vue-tailwind-button>
+      <vue-tailwind-button color="gray"> gray </vue-tailwind-button>
+      <vue-tailwind-button color="red"> red </vue-tailwind-button>
+      <vue-tailwind-button color="yellow"> yellow </vue-tailwind-button>
+      <vue-tailwind-button color="green"> green </vue-tailwind-button>
+      <vue-tailwind-button color="blue"> blue </vue-tailwind-button>
+      <vue-tailwind-button color="indigo"> indigo </vue-tailwind-button>
+      <vue-tailwind-button color="purple"> purple </vue-tailwind-button>
+      <vue-tailwind-button color="pink"> pink </vue-tailwind-button>
+    </div>
+    <div class="grid justify-items-center grid-cols-10 mt-5">
+      <vue-tailwind-button mode="outline" color="black"> black </vue-tailwind-button>
+      <vue-tailwind-button mode="outline" color="white"> white </vue-tailwind-button>
+      <vue-tailwind-button mode="outline" color="gray"> gray </vue-tailwind-button>
+      <vue-tailwind-button mode="outline" color="red"> red </vue-tailwind-button>
+      <vue-tailwind-button mode="outline" color="yellow"> yellow </vue-tailwind-button>
+      <vue-tailwind-button mode="outline" color="green"> green </vue-tailwind-button>
+      <vue-tailwind-button mode="outline" color="blue"> blue </vue-tailwind-button>
+      <vue-tailwind-button mode="outline" color="indigo"> indigo </vue-tailwind-button>
+      <vue-tailwind-button mode="outline" color="purple"> purple </vue-tailwind-button>
+      <vue-tailwind-button mode="outline" color="pink"> pink </vue-tailwind-button>
+    </div>
   </div>
 </template>
--- src/vue-tailwind-button.vue
+++ src/vue-tailwind-button.vue
@@ -10,7 +10,7 @@ export default /*#__PURE__*/ defineComponent({
     },
     mode: {
       type: String,
-      default: "",
+      default: "solid",
     },
     color: {
       type: String,
@@ -31,19 +31,85 @@ export default /*#__PURE__*/ defineComponent({
       disabled: false,
       colors: {
         black: {
-          bg: "bg-black",
-          bgHover: "bg-gray-500",
-          text: "text-white",
+          bg: this.mode == "solid" ? "bg-black" : this.mode == "outline" ? "bg-white" : "",
+          bgHover: this.mode == "solid" ? "bg-gray-500" : this.mode == "outline" ? "bg-black" : "",
+          text: this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-black" : "",
+          textHover:  this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-white" : "",
+          border:  this.mode == "solid" ? "" : this.mode == "outline" ? "border-black" : "",
+          ring: this.mode == "solid" ? "ring-gray-400" : this.mode == "outline" ? "ring-gray-400" : "",
         },
-        white: {},
-        gray: {},
-        red: {},
-        yellow: {},
-        green: {},
-        blue: {},
-        indigo: {},
-        purple: {},
-        pink: {},
+        white: {
+          bg: this.mode == "solid" ? "bg-white" : this.mode == "outline" ? "bg-white" : "",
+          bgHover: this.mode == "solid" ? "bg-gray-500" : this.mode == "outline" ? "bg-gray-500" : "",
+          text: this.mode == "solid" ? "text-black" : this.mode == "outline" ? "text-black" : "",
+          textHover:  this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-white" : "",
+          border:  this.mode == "solid" ? "" : this.mode == "outline" ? "border-white" : "",
+          ring: this.mode == "solid" ? "ring-gray-400" : this.mode == "outline" ? "ring-gray-400" : "",
+        },
+        gray: {
+          bg: this.mode == "solid" ? "bg-gray-500" : this.mode == "outline" ? "bg-white" : "",
+          bgHover: this.mode == "solid" ? "bg-gray-600" : this.mode == "outline" ? "bg-gray-600" : "",
+          text: this.mode == "solid" ? "text-gray-100" : this.mode == "outline" ? "text-gray-500" : "",
+          textHover:  this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-white" : "",
+          border:  this.mode == "solid" ? "" : this.mode == "outline" ? "border-gray-500" : "",
+          ring: this.mode == "solid" ? "ring-gray-400" : this.mode == "outline" ? "ring-gray-400" : "",
+        },
+        red: {
+          bg: this.mode == "solid" ? "bg-red-500" : this.mode == "outline" ? "bg-white" : "",
+          bgHover: this.mode == "solid" ? "bg-red-700" : this.mode == "outline" ? "bg-red-600" : "",
+          text: this.mode == "solid" ? "text-gray-100" : this.mode == "outline" ? "text-red-500" : "",
+          textHover:  this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-white" : "",
+          border:  this.mode == "solid" ? "" : this.mode == "outline" ? "border-red-500" : "",
+          ring: this.mode == "solid" ? "ring-red-400" : this.mode == "outline" ? "ring-red-400" : "",
+        },
+        yellow: {
+          bg: this.mode == "solid" ? "bg-yellow-500" : this.mode == "outline" ? "bg-white" : "",
+          bgHover: this.mode == "solid" ? "bg-yellow-700" : this.mode == "outline" ? "bg-yellow-600" : "",
+          text: this.mode == "solid" ? "text-gray-100" : this.mode == "outline" ? "text-yellow-500" : "",
+          textHover:  this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-white" : "",
+          border:  this.mode == "solid" ? "" : this.mode == "outline" ? "border-yellow-500" : "",
+          ring: this.mode == "solid" ? "ring-gray-400" : this.mode == "outline" ? "ring-gray-400" : "",
+        },
+        green: {
+          bg: this.mode == "solid" ? "bg-green-500" : this.mode == "outline" ? "bg-white" : "",
+          bgHover: this.mode == "solid" ? "bg-green-700" : this.mode == "outline" ? "bg-green-600" : "",
+          text: this.mode == "solid" ? "text-gray-100" : this.mode == "outline" ? "text-green-500" : "",
+          textHover:  this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-white" : "",
+          border:  this.mode == "solid" ? "" : this.mode == "outline" ? "border-green-500" : "",
+          ring: this.mode == "solid" ? "ring-gray-400" : this.mode == "outline" ? "ring-gray-400" : "",
+        },
+        blue: {
+          bg: this.mode == "solid" ? "bg-blue-500" : this.mode == "outline" ? "bg-white" : "",
+          bgHover: this.mode == "solid" ? "bg-blue-700" : this.mode == "outline" ? "bg-blue-600" : "",
+          text: this.mode == "solid" ? "text-gray-100" : this.mode == "outline" ? "text-blue-500" : "",
+          textHover:  this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-white" : "",
+          border:  this.mode == "solid" ? "" : this.mode == "outline" ? "border-blue-500" : "",
+          ring: this.mode == "solid" ? "ring-gray-400" : this.mode == "outline" ? "ring-gray-400" : "",
+        },
+        indigo: {
+          bg: this.mode == "solid" ? "bg-indigo-500" : this.mode == "outline" ? "bg-white" : "",
+          bgHover: this.mode == "solid" ? "bg-indigo-700" : this.mode == "outline" ? "bg-indigo-600" : "",
+          text: this.mode == "solid" ? "text-gray-100" : this.mode == "outline" ? "text-indigo-500" : "",
+          textHover:  this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-white" : "",
+          border:  this.mode == "solid" ? "" : this.mode == "outline" ? "border-indigo-500" : "",
+          ring: this.mode == "solid" ? "ring-gray-400" : this.mode == "outline" ? "ring-gray-400" : "",
+        },
+        purple: {
+          bg: this.mode == "solid" ? "bg-purple-500" : this.mode == "outline" ? "bg-white" : "",
+          bgHover: this.mode == "solid" ? "bg-purple-700" : this.mode == "outline" ? "bg-purple-600" : "",
+          text: this.mode == "solid" ? "text-gray-100" : this.mode == "outline" ? "text-purple-500" : "",
+          textHover:  this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-white" : "",
+          border:  this.mode == "solid" ? "" : this.mode == "outline" ? "border-purple-500" : "",
+          ring: this.mode == "solid" ? "ring-gray-400" : this.mode == "outline" ? "ring-gray-400" : "",
+        },
+        pink: {
+          bg: this.mode == "solid" ? "bg-pink-500" : this.mode == "outline" ? "bg-white" : "",
+          bgHover: this.mode == "solid" ? "bg-pink-700" : this.mode == "outline" ? "bg-pink-600" : "",
+          text: this.mode == "solid" ? "text-gray-100" : this.mode == "outline" ? "text-pink-500" : "",
+          textHover:  this.mode == "solid" ? "text-white" : this.mode == "outline" ? "text-white" : "",
+          border:  this.mode == "solid" ? "" : this.mode == "outline" ? "border-pink-500" : "",
+          ring: this.mode == "solid" ? "ring-gray-400" : this.mode == "outline" ? "ring-gray-400" : "",
+        },
       },
     };
   },
@@ -62,26 +128,25 @@ export default /*#__PURE__*/ defineComponent({
           (this.hoverTextColor ?? "white") != "white" ? "-600" : ""
         }
         `;
+
+
+        this.css = `focus:${this.colors[this.color].ring} ${this.colors[this.color].border} ${this.colors[this.color].bg} ${this.colors[this.color].text} hover:${this.colors[this.color].bgHover} hover:${this.colors[this.color].textHover} border focus:outline-none focus:ring-4 py-1 px-2 rounded font-medium mx-3 transition duration-200 each-in-out border`;
         break;
-      case `rounded`:
+      case "rounded":
         this.css = `focus:outline-none text-${this.textColor}-600 text-sm py-1 px-2 rounded-full border border-${this.color}-600 hover:bg-${this.color}-600 hover:text-white`;
         break;
-      case `flat`:
+      case "flat":
         this.css = `focus:outline-none text-${this.textColor}-600 text-sm py-1 px-2 rounded hover:bg-${this.color}-200`;
         break;
-      case `flat-rounded`:
+      case "flat-rounded":
         this.css = `bg-${this.color}-500 focus:outline-none text-white text-sm py-1 px-2 rounded-full border border-${this.color}-600 hover:bg-${this.color}-600 hover:text-white`;
         break;
-      case `disabled`:
+      case "disabled":
         this.css = `focus:outline-none text-white text-sm py-1 px-2 rounded bg-${this.color}-300`;
         this.disabled = true;
         break;
-      default:
-        this.css = `${this.colors[this.color].bg} ${
-          this.colors[this.color].text
-        } hover:${
-          this.colors[this.color].bgHover
-        } py-1 px-2 rounded font-medium mx-3 transition duration-200 each-in-out`;
+      case "solid":
+        this.css = `${this.colors[this.color].bg} ${this.colors[this.color].text} hover:${this.colors[this.color].bgHover} hover:${this.colors[this.color].textHover} py-1 px-2 rounded font-medium mx-3 transition duration-200 each-in-out`;
         break;
     }
   },
